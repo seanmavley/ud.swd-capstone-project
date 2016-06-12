d8904ee4eb1d3c148d64879513a4695eac7cd8ac
Rexford <seanmavley@gmail.com>
2016-06-12 22:09:24 +0000

    updating user profile

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -1,5 +1,43 @@
 angular.module('codeSide')
-  
-  .controller('AdminController', function($scope, currentAuth) {
-    $scope.user = currentAuth;
-  })
+
+.controller('AdminController', function($scope, $firebaseObject, currentAuth, Auth) {
+  var ref = firebase.database().ref();
+  var userData = $firebaseObject(ref.child('users').child(currentAuth.uid));
+  // prepopulate user form if any
+  userData.$loaded()
+    .then(function() {
+      console.log(userData);
+      $scope.formData = userData;
+      $scope.formData.dob = new Date(userData.dob);
+    })
+
+  $scope.updateUser = function() {
+    if (!$scope.formData.fullname) {
+      $scope.error = 'Please add full name'
+    } else {
+      console.log($scope.formData);
+      userData.$loaded()
+        .then(function() {
+          ref
+            .child('users')
+            .child(currentAuth.uid)
+            .set({
+              fullname: $scope.formData.fullname,
+              dob: new Date($scope.formData.dob).getTime(),
+              country: $scope.formData.country
+            })
+        })
+      $scope.message = 'User profile saved';
+    }
+  }
+
+  $scope.user = currentAuth;
+
+  $scope.updatePassword = function() {
+    Auth.$updatePassword($scope.newPassword).then(function() {
+      $scope.message = 'Successful';
+    }).catch(function(error) {
+      console.error("Error: ", error);
+    });
+  }
+})
