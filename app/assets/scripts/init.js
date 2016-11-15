5b1faadc1ef0cbcbf7afadc4c99a54f0088cdb8e
Rexford <seanmavley@gmail.com>
2016-11-15 23:46:48 +0000

    admin page complete

--- app/assets/scripts/init.js
+++ app/assets/scripts/init.js
@@ -1,40 +1,42 @@
-toastr.options = {
-  "closeButton": false,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": false,
-  "positionClass": "toast-top-right",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
-
-Offline.options = {
-  reconnect: {
-    // Try to reconnect every 30 seconds
-    initialDelay: 30
+$(document).ready(function() {
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
   }
-};
 
-Offline.on('down', function() {
-  // clear any toastr notification on screen
-  // already
-  toastr.clear();
-  toastr.warning('You are not connected to the internet', 'Offline mode', {
-    timeOut: 10000
+  Offline.options = {
+    reconnect: {
+      // Try to reconnect every 30 seconds
+      initialDelay: 30
+    }
+  };
+
+  Offline.on('down', function() {
+    // clear any toastr notification on screen
+    // already
+    toastr.clear();
+    toastr.warning('You are not connected to the internet', 'Offline mode', {
+      timeOut: 10000
+    });
   });
-});
 
-Offline.on('up', function() {
-  toastr.clear()
-  toastr.success('Network is back', 'Now online', {
-    timeOut: 8000
+  Offline.on('up', function() {
+    toastr.clear()
+    toastr.success('Network is back', 'Now online', {
+      timeOut: 8000
+    });
   });
 });
