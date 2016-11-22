d7ff032d378f47bea9d9a442e7ca4810fe199fd2
Rexford <seanmavley@gmail.com>
2016-11-22 23:31:22 +0000

    finishing touches for deployment, without offline capabilities

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -2,61 +2,18 @@ angular.module('codeSide')
 
 .controller('HomeController', ['$scope', 'Auth', 'DatabaseRef', '$firebaseArray', '$window',
   function($scope, Auth, DatabaseRef, $firebaseArray, $window) {
+    var codeDataRef = DatabaseRef.child('codes');
+    var query = codeDataRef.orderByChild("createdAt").limitToLast(25);
 
-    var doRequest = function() {
-      $scope.offline = false;
-      console.log($scope.offline);
+    var list = $firebaseArray(query);
 
-      var codeDataRef = DatabaseRef.child('codes');
-      var query = codeDataRef.orderByChild("createdAt").limitToLast(25);
-
-      var list = $firebaseArray(query);
-
-      list.$loaded()
-        .then(function(data) {
-          $scope.list = data;
-          localforage.setItem('homepage', JSON.stringify(data)).then(function(res) {
-              toastr.success('Homepage Database Offline', 'Content taken offline');
-            })
-            .catch(function(error) {
-              console.log(error);
-            })
-        })
-        .catch(function(error) {
-          toastr.error(error.message);
-        })
-    }
-
-    if (Offline.state === 'up') {
-      $scope.offline = false;
-      doRequest();
-    } else {
-      $scope.offline = true;
-      doOffline();
-    };
-
-    var doOffline = function() {
-      $scope.offline = true;
-      $scope.$apply();
-      console.log($scope.offline);
-      // TODO:
-      // clean the retrieved stringified data
-      // currently works, but look for better way?
-      localforage.getItem('homepage')
-        .then(function(data) {
-          console.log(data);
-          $scope.list = data;
-        })
-    }
-
-    Offline.on('down', function() {
-      doOffline();
-    });
-
-
-    Offline.on('up', function() {
-      doRequest();
-    });
+    list.$loaded()
+      .then(function(data) {
+        $scope.list = data;
+      })
+      .catch(function(error) {
+        toastr.error(error.message);
+      })
 
   }
 ])
